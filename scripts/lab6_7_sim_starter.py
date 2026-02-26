9546e4e10f0315113a6ba52f587125db2d8df150
HarryYang2369 <y.yang-343@sms.ed.ac.uk>
2026-02-26 11:13:00 -0800

    try to check wall

--- scripts/lab6_7_sim_starter.py
+++ scripts/lab6_7_sim_starter.py
@@ -380,7 +380,9 @@ class ObstacleAvoidingWaypointController:
             self.robot_ctrl_pub.publish(Twist())
             return
         ranges = list(self.laserscan.ranges)
-        front_raw = ranges[0:25] + ranges[-25:]
+        # Check right/front side for obstacles (since IR camera is on left)
+        # Indices 250-290 represent ~250-290 degrees (right side of robot)
+        front_raw = ranges[250:290]
         front =[r for r in front_raw if (r >self.laserscan.range_min and r <self.laserscan.range_max)]
         front_min = min(front) if len(front) > 0 else inf
         if self.ir_distance is None:
